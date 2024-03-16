47e3bbc1784b0d17cbce10b3813f8d690f49ea5d
DaymensCode <Daymenscode@gmail.com>
2024-03-16 02:05:40 -0400

    started setup for Navigation component

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,2 +1,19 @@
 // will use react-router-doms Link component to conditionally render 
 //different parts of my Portfolio
+
+//These two imports will give access to utilities/methods that 
+//can be used the router defined in main.jsx
+import { Link, useLocation } from 'react-router-dom';
+
+function CreateNavigationTabs() {
+
+
+
+
+
+
+}
+
+
+
+export default CreateNavigationTabs;
