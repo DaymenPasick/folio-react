bc3d8f617563d6b0e8ec980e72de79f3e8be529b
DaymensCode <Daymenscode@gmail.com>
2024-03-16 15:13:18 -0400

    generated 5 more placeholder projects in pastProjects.js to edit later

--- src/pastProjects.js
+++ src/pastProjects.js
@@ -9,6 +9,41 @@ const pastProjects = [
     imgRef: "./assets/pastProjectImgs/project1img1",
     deployedLink: "https://daymenpasick.github.io/api-powered-weather-app/",
     gitHubLink: "https://github.com/DaymenPasick/api-powered-weather-app"
+},
+{
+    projectNumber: 2,
+    title: "Project 2",
+    imgRef: "./assets/pastProjectImgs/project1img1",
+    deployedLink: "https://daymenpasick.github.io/api-powered-weather-app/",
+    gitHubLink: "https://github.com/DaymenPasick/api-powered-weather-app"
+},
+{
+    projectNumber: 3,
+    title: "Project 3",
+    imgRef: "./assets/pastProjectImgs/project1img1",
+    deployedLink: "https://daymenpasick.github.io/api-powered-weather-app/",
+    gitHubLink: "https://github.com/DaymenPasick/api-powered-weather-app"
+},
+{
+    projectNumber: 4,
+    title: "Project 4",
+    imgRef: "./assets/pastProjectImgs/project1img1",
+    deployedLink: "https://daymenpasick.github.io/api-powered-weather-app/",
+    gitHubLink: "https://github.com/DaymenPasick/api-powered-weather-app"
+},
+{
+    projectNumber: 5,
+    title: "Project 5",
+    imgRef: "./assets/pastProjectImgs/project1img1",
+    deployedLink: "https://daymenpasick.github.io/api-powered-weather-app/",
+    gitHubLink: "https://github.com/DaymenPasick/api-powered-weather-app"
+},
+{
+    projectNumber: 6,
+    title: "Project 6",
+    imgRef: "./assets/pastProjectImgs/project1img1",
+    deployedLink: "https://daymenpasick.github.io/api-powered-weather-app/",
+    gitHubLink: "https://github.com/DaymenPasick/api-powered-weather-app"
 }
 
 
