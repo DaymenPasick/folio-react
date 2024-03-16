a43af97817de445cc00f800d68f4e9b8109f87aa
DaymensCode <Daymenscode@gmail.com>
2024-03-16 01:59:36 -0400

    initial setup for App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+//Bringing in Outlet import that will facilitate  page navigation
+//using the page URL's
+import { Outlet } from 'react-router-dom';
+
+//Allows use of component that will be created later
+import Nav from './components/Navigation';
+
+function App() {
+    //will invoke Nav and Outlet from imports 
+  return (
+    <>
+      <Nav />
+      <main className="mx-3">
+        <Outlet />
+      </main>
+    </>
+  );
+}
+
+//Will allow for use of setup App elsewhere in code
+export default App;
