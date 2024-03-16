979d3a43b233fca82477e305aea786551cb21005
DaymensCode <Daymenscode@gmail.com>
2024-03-16 02:25:04 -0400

    added guide comments to Error.jsx

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,5 +1,8 @@
+//Will import functionality for route pathing error handling
 import { useRouteError } from 'react-router-dom';
 
+//this code will handle errors in/with route paths 
+//if any should occur
 export default function ErrorPage() {
   const error = useRouteError();
   console.error(error);
