235d4713ed1acdd7e96a0c72e0b98bff0fef8424
DaymensCode <Daymenscode@gmail.com>
2024-03-16 02:32:18 -0400

    removed some comments and added guide comments within Navigation.jsx

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -10,13 +10,15 @@ function CreateNavigationTabs() {
     //sets variable for active page to be used in the return() below
     const currentPage = useLocation().pathname;
 
+    
+    //in the return below, terinary operators will be used to check 
+    //the current page, and use bootsrap import from main.jsx
+    //to utilize the nav-link class functionality for dynamic tabs
     return (
       <ul className="nav nav-tabs">
         <li className="nav-item">
           <Link
             to="/"
-            // This is a conditional (ternary) operator that checks to see if the current page is "Home"
-            // If it is, we set the current page to 'nav-link-active', otherwise we set it to 'nav-link'
             className={currentPage === '/' ? 'nav-link active' : 'nav-link'}
           >
             Home
@@ -25,7 +27,6 @@ function CreateNavigationTabs() {
         <li className="nav-item">
           <Link
             to="/About"
-            // Check to see if the currentPage is `About`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
             className={currentPage === '/About' ? 'nav-link active' : 'nav-link'}
           >
             About
@@ -34,7 +35,6 @@ function CreateNavigationTabs() {
         <li className="nav-item">
           <Link
             to="/Contact"
-            // Check to see if the currentPage is `Contact`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
             className={currentPage === '/Contact' ? 'nav-link active' : 'nav-link'}
           >
             Contact
@@ -43,7 +43,6 @@ function CreateNavigationTabs() {
         <li className="nav-item">
           <Link
             to="/Portfolio"
-            // Check to see if the currentPage is `Blog`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
             className={currentPage === '/Blog' ? 'nav-link active' : 'nav-link'}
           >
             Portfolio
@@ -52,7 +51,6 @@ function CreateNavigationTabs() {
         <li className="nav-item">
           <Link
             to="/Resume"
-            // Check to see if the currentPage is `Blog`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
             className={currentPage === '/Blog' ? 'nav-link active' : 'nav-link'}
           >
             Resume
