e31182d245672cef2466f9b74d112825c5a6357a
DaymensCode <Daymenscode@gmail.com>
2024-03-16 03:20:46 -0400

    messing with some styles to get a feel for design direction

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,29 @@ import { Outlet } from 'react-router-dom';
 //Allows use of component that will be created later
 import Nav from './components/Navigation';
 
+
+//eventually will make this into a seperate stylesheet
+//for neater code
+const styles = {
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+    height: '100vh',
+    backgroundColor: 'grey'
+  },
+} 
+
+
 function App() {
     //Will invoke Nav and Outlet from imports 
   return (
-    <>
+    <div >
       <Nav />
-      <main className="mx-3">
+      {/* playing with styleing...my true enemy xD */}
+      <main className="mx-3" style = {styles.container}>
         <Outlet />
       </main>
-    </>
+    </div>
   );
 }
 
