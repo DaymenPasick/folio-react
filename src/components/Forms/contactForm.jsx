47afdf695f1fb872c9abe31b58d23339cfa96e49
DaymensCode <Daymenscode@gmail.com>
2024-03-17 11:53:05 -0400

    added basic setup for contactForm component, as well as needed imports

---
+++ src/components/Forms/contactForm.jsx
@@ -0,0 +1,23 @@
+//Importing useState to utilize its hook functionality
+import { useState } from 'react';
+
+//Importing the validateEmail helper function to
+//use within the form
+import { validateEmail } from '../../utils/helpers';
+
+
+function Form() {
+
+
+
+
+    return (
+        <div>
+            <p>boop</p>
+        </div>
+    );
+
+};
+
+//Will export the Form function for use elsewhere in code
+export default Form
---
+++ src/utils/helpers.js
@@ -0,0 +1,6 @@
+//Export function that will use regex to check for valid emails
+
+export function validateEmail(email) {
+  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
