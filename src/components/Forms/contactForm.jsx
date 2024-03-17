32449e1c49ec1a4eb9994d266f3bf4a99d2f55e2
DaymensCode <Daymenscode@gmail.com>
2024-03-17 13:08:32 -0400

    added functionality for handleFormSubmit

--- src/components/Forms/contactForm.jsx
+++ src/components/Forms/contactForm.jsx
@@ -11,7 +11,7 @@ function Form() {
     //Setting up useState variables and declaring their functions
     const [contactName, setContactName] = useState('');
     const [email, setEmail] = useState('');
-    const [userName, setUserName] = useState('');
+    const [contactMessage, setContactMessage] = useState('');
     //Similar to above setup, but for handling form errors
     const [errorMessage, setErrorMessage] = useState('');
     
@@ -25,11 +25,27 @@ function Form() {
     }
 
 
-    //Arrow function that will listen for 
+    //Arrow function that will listen for
     //clicking on submit button, and handle submitted data
     //when we set it up with the clickEvent later
     const handleFormSubmit =(e) =>{
+        e.preventDefault();
 
+        //Will take in the form-entered username and email
+        //and check if email is valid/username isn't empty
+        if (!validateEmail(email) || !contactName) {
+            setErrorMessage('Email or username is invalid');
+
+            return;
+          }
+
+          //If the above checks pass, the useState variables
+          //will be reset to empty strings and 
+          //alert message will diplay
+          setContactName('');
+          setEmail('');
+          setContactMessage('');
+          alert(`Thanks for contacting me ${userName}. I'll respond ASAP! `);
     }
     
     return (
