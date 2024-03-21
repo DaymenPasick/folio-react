e4fdb36bf857f6481400a901c054fd74a97381ee
DaymensCode <Daymenscode@gmail.com>
2024-03-21 02:01:44 -0400

    filled out more data within the Resume page

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -7,17 +7,24 @@ export default function Resume() {
         <a href={resumeDownload} target="_blank" download>Download My Resume</a>
         <h2>Front-End Proficies</h2>
           <ul>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
+            <li>React</li>
+            <li>Javascript</li>
+            <li>JQuery</li>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>Bootstrap</li>
+            <li>Responsive Design</li>
           </ul>
         <h2>Back-End Proficies</h2>
           <ul>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
+            <li>MySQL, Sequelize</li>
+            <li>MongoDB, Mongoose</li>
+            <li>GraphQL</li>
+            <li>API's</li>
+            <li>REST</li>
+            <li>Node</li>
+            <li>Express</li>
+            <li>Node</li>
           </ul>
 
       </div>
