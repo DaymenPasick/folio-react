fa6517ed96602b7326b8f1ff367fa6ea6f3b2532
DaymensCode <Daymenscode@gmail.com>
2024-03-21 01:10:58 -0400

    added img links in footer for github, twitter, and linkedin

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,6 @@
 import gitIcon from '../../public/assets/GitHubIconLink.png'
 import linkedIcon from '../../public/assets/LinkedInIconLink.png'
+import twitterIcon from '../../public/assets/TwitterIconLink.png'
 
 
 export default function Footer(props) {
@@ -11,10 +12,10 @@ export default function Footer(props) {
             <img src={gitIcon} alt="GitHub Icon" />
         </a>
         <a href="https://www.linkedin.com/in/daymen-pasick-a956692a2/" target="_blank">
-            <img src={linkedIcon} alt="LinkedIn" />
+            <img src={linkedIcon} alt="LinkedIn Icon" />
         </a>
-        <a href="https://github.com/DaymenPasick" target="_blank">
-            <img src={gitIcon} alt="GitHub Icon" />
+        <a href="https://twitter.com/DaymensCode" target="_blank">
+            <img src={twitterIcon} alt="Twitter Icon" />
         </a>
     </div>
 
