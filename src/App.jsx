771eb9832fc4c976897bb7ca5f94a58023265d48
DaymensCode <Daymenscode@gmail.com>
2024-03-24 18:11:18 -0400

    test for mobile

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
         {/* </div> */}
           <Header />
       </div>
-      <main className='container myContainer rounded-bottom rounded-end' >
+      <main className='container myContainer rounded-bottom rounded-end rounded-start' >
 
         <Outlet />
 
