8e2a1824b6b00a43ec85c7cc640cc2c975ac7d5c
DaymensCode <Daymenscode@gmail.com>
2024-03-25 00:15:07 -0400

    ok done for real tonight xD

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,22 +8,22 @@ export default function Footer(props) {
  return (
     <div className='container fixed-bottom'>
      <div className='row footBorder  '>
-       <div className='col-3 col-sm-4 text-center mb-'> 
+       <div className='col-3 col-sm-4 text-center '> 
     {/* <div className='container fixed-bottom'>
      <div className='row footBorder  mb-sm-5 pb-sm-5 mb-lg-5 pb-lg-5'>
        <div className='col-3 col-sm-4 text-center  mb-sm-5 mb-lg-5 '>  */}
         <a className='' href="https://github.com/DaymenPasick" target="_blank">
-            <img className='img-fluid'  src={gitIcon} alt="GitHub Icon" />
+            <img className='img-fluid imgShadow'  src={gitIcon} alt="GitHub Icon" />
         </a>
        </div> 
        <div className='col-5 col-sm-4 text-center '>
         <a href="https://www.linkedin.com/in/daymen-pasick-a956692a2/" target="_blank">
-            <img className='img-fluid'  src={linkedIcon} alt="LinkedIn Icon" />
+            <img className='img-fluid imgShadowLinked'  src={linkedIcon} alt="LinkedIn Icon" />
         </a>
         </div>
         <div className='col-3 col-sm-4 text-center '> 
         <a href="https://twitter.com/DaymensCode" target="_blank">
-            <img className='img-fluid'  src={twitterIcon} alt="Twitter Icon" />
+            <img className='img-fluid imgShadow'  src={twitterIcon} alt="Twitter Icon" />
         </a>
         </div>
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ export default function Header(props) {
        <div className='container'>
          <div className='row pt-3 '>
          <div className=" col-sm-4 d-flex justify-content-evenly">
-         <h1 className='nameHeader '><span className='spanHeader'>Day</span>men</h1>
+         <h1 className='nameHeader letterShadow'><span className='spanHeader'>Day</span>men</h1>
             </div>
          
          <div className=" col-sm-8 d-flex justify-content-end ">
