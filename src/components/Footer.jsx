fceff590f2cdc470a58bf4cd79e6e46274619a36
DaymensCode <Daymenscode@gmail.com>
2024-03-24 22:25:11 -0400

    just kidding still working xD updateded sizing on footer in attempts for it to scale better

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,18 +7,18 @@ import twitterIcon from '../../public/assets/TwitterIconLink.png'
 export default function Footer(props) {
  return (
     <div className='container fixed-bottom'>
-     <div className='row footBorder'>
-       <div className='col-sm-4 text-center  mb-md-1 mb-sm-3 mb-0'> 
+     <div className='row footBorder mb-0 mb-sm-5 pb-sm-5' >
+       <div className='col-3 col-sm-4 text-center mb-sm-5 '> 
         <a className='' href="https://github.com/DaymenPasick" target="_blank">
             <img className='img-fluid'  src={gitIcon} alt="GitHub Icon" />
         </a>
        </div> 
-       <div className='col-sm-4 text-center'>
+       <div className='col-5 col-sm-4 text-center '>
         <a href="https://www.linkedin.com/in/daymen-pasick-a956692a2/" target="_blank">
             <img className='img-fluid'  src={linkedIcon} alt="LinkedIn Icon" />
         </a>
         </div>
-        <div className='col-sm-4 text-center'> 
+        <div className='col-3 col-sm-4 text-center '> 
         <a href="https://twitter.com/DaymensCode" target="_blank">
             <img className='img-fluid'  src={twitterIcon} alt="Twitter Icon" />
         </a>
