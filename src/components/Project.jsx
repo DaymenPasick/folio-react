96f3fec58303ecd95c9492beddc9f228d4661716
DaymensCode <Daymenscode@gmail.com>
2024-03-24 22:04:20 -0400

    finished initial positioning of portfolio, and got slight border around About Me Pic

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -4,14 +4,13 @@ import ProjectListItems from "./ProjectListItems"
 //Allows for dynamic generation and export of (.mapped) past projects
 export default function Project({pastProjects}) {
     return(
-        // <div className="container blocked">
-        <div className="container d-flex ">
-        <div className='row text-center d-flex justify-content-center'>   
+        <div className="container">
+        <div className='row text-center d-flex'>   
             {pastProjects.map((pastProject) => (      
              <ProjectListItems pastProject={pastProject}
                           key={pastProject.projectNumber}/> 
            ))}
-               </div>
+        </div>
         </div>
     )
 
