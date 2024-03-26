f4b34b13fdce8975ff77a290dff65175cc563ed6
DaymensCode <Daymenscode@gmail.com>
2024-03-25 23:31:13 -0400

    About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,9 +4,9 @@ import Avatar from '../../public/assets/projectAvatarSamp.png'
 //Setup for About page
 export default function About() {
     return (
-      <div className='container '>
-       <div className='row text-center pt-5 '>
-        <div className="col  ">
+      <div className='container'>
+       <div className='row pt-5 text-center'>
+        <div className='col'>
           <h1 className='pageHeaderText'>About Me</h1>
           </div>
        </div>
@@ -14,20 +14,23 @@ export default function About() {
 
        <div className='row my-5'>
          <div className='col pt-3 '>
-           <img className='img-fluid mx-auto d-block circle ' src={Avatar} alt="profile avatar of a man wearing sunglasses" />
+           <img className='img-fluid mx-auto d-block circle' 
+                src={Avatar} 
+                alt="profile avatar of a man wearing sunglasses"
+             />
          </div> 
        </div>
 
-          <div className='row '>
-         <div className='col text-center my-3 pt-4'>
-           <p className='generalText mx-5 '>
+       <div className='row'>
+         <div className='col my-3 pt-4 text-center'>
+           <p className='mx-5 generalText'>
             Hey hey!
             Budding developer here taking the learning journey
             day by day. 
             Also I REALLY like cookies.
            </p>
-           </div>
          </div>
+        </div>
       </div>
     );
   }
