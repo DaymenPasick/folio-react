e832f96e22a30c4618556ff68915b80c6f2159f1
DaymensCode <Daymenscode@gmail.com>
2024-03-25 23:41:30 -0400

    cleaned up Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,17 +5,16 @@ import Nav from './Navigation';
 export default function Header(props) {
     return (
        <div className='container'>
-         <div className='row pt-3 '>
-         <div className=" col-sm-4 d-flex justify-content-evenly">
-         <h1 className='nameHeader letterShadow'><span className='spanHeader'>Day</span>men</h1>
+         <div className='row pt-3'>
+            <div className='col-sm-4 d-flex justify-content-evenly'>
+               <h1 className='nameHeader letterShadow'><span className='spanHeader'>Day</span>men</h1>
             </div>
-         
-         <div className=" col-sm-8 d-flex justify-content-end ">
-          <Nav />
-          </div>
-
             
-          </div>
+            <div className='col-sm-8 d-flex justify-content-end'>
+               <Nav />
+            </div>
+            
+         </div>
        </div>
     );
 }    
