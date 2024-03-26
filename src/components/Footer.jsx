187b95bd87c8e8e2ceb1845302e6acb2fadaf58f
DaymensCode <Daymenscode@gmail.com>
2024-03-25 23:44:28 -0400

    cleaned up Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,25 +6,27 @@ import twitterIcon from '../../public/assets/TwitterIconLink.png'
 //Structure for Footer
 export default function Footer(props) {
  return (
-    <div className='container fixed-bottom '>
-     <div className='row footBorder d-flex justify-content-center mb-2'>
-       <div className='col-3  text-center '> 
-        <a className='' href="https://github.com/DaymenPasick" target="_blank">
-            <img className='img-fluid imgShadow'  src={gitIcon} alt="GitHub Icon" />
-        </a>
-       </div> 
-       <div className='col-5 col-md-6 text-center '>
+    <div className='container fixed-bottom'>
+      <div className='row mb-2 d-flex justify-content-center footBorder'>
+        <div className='col-3 text-center'> 
+            <a href="https://github.com/DaymenPasick" target="_blank">
+                <img className='img-fluid imgShadow'  src={gitIcon} alt="GitHub Icon" />
+            </a>
+        </div>
+
+       <div className='col-5 col-md-6 text-center'>
         <a href="https://www.linkedin.com/in/daymen-pasick-a956692a2/" target="_blank">
             <img className='img-fluid imgShadowLinked'  src={linkedIcon} alt="LinkedIn Icon" />
         </a>
-        </div>
-        <div className='col-3  text-center '> 
+       </div>
+
+       <div className='col-3 text-center'> 
         <a href="https://twitter.com/DaymensCode" target="_blank">
             <img className='img-fluid imgShadow'  src={twitterIcon} alt="Twitter Icon" />
         </a>
-        </div>
+       </div>
 
-     </div> 
+      </div> 
     </div>
  );
 }
